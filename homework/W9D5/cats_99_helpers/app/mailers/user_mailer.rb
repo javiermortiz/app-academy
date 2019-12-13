bd319bb182a1ed0dd665c28705d3208988a73e98
Javier Ortiz <javier.ortiz989@gmail.com>
2019-12-12 16:31:19 -0800

    Fix email function

--- homework/W9D5/cats_99_helpers/app/mailers/user_mailer.rb
+++ homework/W9D5/cats_99_helpers/app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
     default from: 'from@example.com'
 
     def welcome_email(user)
-        
+        @user = user
+        @url = 'http://example.com/login'
+        mail(to: @url, subject: 'Welcome to My Awesome Site')
     end 
 end
