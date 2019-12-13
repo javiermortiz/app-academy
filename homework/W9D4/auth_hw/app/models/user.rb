3224676144cb937ee127348ca3890b5e8e61ffa4
Javier Ortiz <javier.ortiz989@gmail.com>
2019-12-12 16:25:04 -0800

    Add homework for W9D5

--- homework/W9D4/auth_hw/app/models/user.rb
+++ homework/W9D4/auth_hw/app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  validates :password_digest, presence: { message: 'Password can\'t be blank'}
-  validates :session_token, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true  
+  validates :session_token, presence: true, uniqueness: true  
+  validates :password_digest, presence: { message: "Password can't be blank" }
   validates :password, length: { minimum: 6, allow_nil: true }
-  before_validation :ensure_session_token
+  before_validation :ensure_session_token 
 
   attr_reader :password 
 
+
+
+
+
+
+
+  
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
