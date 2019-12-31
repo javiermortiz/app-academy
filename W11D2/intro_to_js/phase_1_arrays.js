31a7ddc021ab592447f47826139c9ea0a2dba592
Javier Ortiz <javier.ortiz989@gmail.com>
2019-12-31 12:54:21 -0800

    First 3 projects of the day

---
+++ W11D2/intro_to_js/phase_1_arrays.js
@@ -0,0 +1,36 @@
+Array.prototype.uniq = function() {
+    let unique = [];
+    for (let i = 0; i < this.length; i++) {
+        if (!unique.includes(this[i])) {
+            unique.push(this[i])
+        }
+    }
+    return unique;
+}
+
+Array.prototype.twoSum = function () {
+  let result = [];
+  for (let i = 0; i < this.length-1; i++) {
+    for (let j = i + 1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        result.push([i, j]);
+      }
+    }
+  }
+  return result;
+}
+
+Array.prototype.transpose = function () {
+    let transArr = [];
+    for (let i = 0; i < this[0].length; i++) { 
+        let sub = [];
+        for (let j = 0; j < this.length; j++) {
+            sub.push(this[j][i]);
+        }
+        transArr.push(sub);
+    }
+    return transArr;
+}
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log([[1, 2, 3], [4, 5, 6], [7, 8, 9]].transpose());
+//  => [[1,4], [2,5], [3,6]]
