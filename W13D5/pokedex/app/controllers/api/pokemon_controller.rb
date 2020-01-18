00d0616823447ba1988749ff3dfd99617fbeb1a3
Javier Ortiz <javier.ortiz989@gmail.com>
2020-01-18 09:13:08 -0800

    Add Pokedex

---
+++ W13D5/pokedex/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,12 @@
+class Api::PokemonController < ApplicationController
+  def index
+    @pokemons = Pokemon.includes(:items).all 
+    render :index
+  end
+
+  def show
+    @pokemon = Pokemon.includes(:items).find(params[:id])
+    render :show
+  end 
+
+end
---
+++ W13D5/pokedex/frontend/actions/pokemon_actions.js
@@ -0,0 +1,15 @@
+export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON"
+import * as APIUtil from '../util/api_util'
+
+export const requestAllPokemon = () => (dispatch) => (
+    APIUtil.fetchAllPokemon()
+        .then(pokemon => dispatch(receiveAllPokemon(pokemon)))
+);
+
+export const receiveAllPokemon = pokemon => ({
+    type: RECEIVE_ALL_POKEMON,
+    pokemon
+});
+
+window.requestAllPokemon = requestAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;
