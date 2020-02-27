0f2708830645d59dbc7549e3ab59fcd58364e692
Javier Ortiz <javier.ortiz989@gmail.com>
2020-02-26 19:27:13 -0800

    Complete hw

---
+++ homework/W16D4/apollo_frontend/client/src/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Register from "./Register";
+
+const App = () => <Route path="/" component={Register} />;
+
+export default App;
